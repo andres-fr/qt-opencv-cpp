42c86ef88997b0a1fc5610b6870cdeda7109b985
andres-fr <fr_andres@msn.com>
2017-11-24 23:22:48 +0100

    barchart now responds to intensity on first bar

--- include/integrated_gui.hpp
+++ include/integrated_gui.hpp
@@ -18,17 +18,15 @@ class IntegratedGui : public QtWebCam {
   Q_OBJECT
 protected slots:
   void updateRgbChart() {
-    xxx = (xxx+1)%100;
-    setPixmapScaled(this->label_2, chart_.getChart(xxx, 110, 90));
+    double x = cv::sum(cv_mat_)[0]/500000;
+    setPixmapScaled(this->label_2, chart_.getChart(x, 110, 90));
   }
 protected:
   QtRgbChart chart_;
-  size_t xxx;
 public:
   IntegratedGui(QMainWindow* w, const size_t cam_idx=0)
     : QtWebCam(w, cam_idx),
       chart_(400, 170, 3, Qt::black, QColor(200,200,200), 30, 0.8, 50, -10){
-    xxx = 0;
     QObject::connect(&timer_, SIGNAL(timeout()), this, SLOT(updateRgbChart()));
     updateRgbChart();
   }
